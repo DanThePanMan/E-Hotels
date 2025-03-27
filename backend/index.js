121d1d14a19b8978fff697a73e2e1590a4742e1e
DanThePanMan <danielchenschw@gmail.com>
2025-03-27 18:49:15 -0400

    Set up backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const { pool } = require("./db");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000 and listening for requests...");
+    pool.query("SELECT 1", (err, res) => {
+        if (err) {
+            console.error("Database connection failed:", err);
+        } else {
+            console.log("Database is connected and operational.");
+        }
+    });
 });
